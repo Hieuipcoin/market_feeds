aa0034f3d108bb08d6c8a5f628c7b89e80912ba6
Riki <hieult@ipcoin.vn>
2019-02-27 17:32:25 +0700

    first commit

---
+++ lib/market_feeds.ex
@@ -0,0 +1,18 @@
+defmodule MarketFeeds do
+  @moduledoc """
+  Documentation for MarketFeeds.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MarketFeeds.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/market_feeds_test.exs
@@ -0,0 +1,8 @@
+defmodule MarketFeedsTest do
+  use ExUnit.Case
+  doctest MarketFeeds
+
+  test "greets the world" do
+    assert MarketFeeds.hello() == :world
+  end
+end
